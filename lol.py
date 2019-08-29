0a5c5e94643b89420169e96bc4b30e67dd05a6c5
Bitovito <44344453+Bitovito@users.noreply.github.com>
2019-08-29 18:48:42 -0400

    omegalul
    arreglé unos parentesis

--- lol.py
+++ lol.py
@@ -1,11 +1,10 @@
 import re
-
 entrada = 'x'
 
-#select = re.compile(r'SELECT\s([A-Z]+(,\s\w+)*|\*)\sFROM\s[A-Z]+(\s(INNER\sJOIN)\s([A-Z]+))?(\(sWHERE)\s([A-Z]+\s=\s\w+)((AND|OR)\s([A-Z]+\s=\s\w+))*)?(\s(ORDER\sBY)\s([A-Z]\s(ASC|DESC)))?')
+select = re.compile(r'SELECT\s([A-Z]+(,\s\w+)*|\*)\sFROM\s([A-Z]+)(\s(INNER\sJOIN)\s([A-Z]+))?((\sWHERE\s([A-Z]+\s=\s\w+)((AND|OR)\s([A-Z]+\s=\s\w+))*)?(\s(ORDER\sBY)\s([A-Z]\s(ASC|DESC))))?')
 insert = re.compile(r'INSERT\sINTO\s([A-Z]+)\s\(([A-Z]+(,[A-Z]+)*)\)\sVALUES\s\((\w+(,\w+)*)\);')
-update = re.compile(r'UPDATE\s([A-Z]+)\sSET\s(([A-Z]+\s=\s\w+)(,[A-Z]+\s=\s\w+)*)\sWHERE\s([A-Z]+\s=\s\w+)(\sAND\s[A-Z]+\s=\s\w+|\sOR\s[A-Z]\s=\s\w+)*;')
-
+update = re.compile(r'UPDATE\s([A-Z]+)\sSET\s([A-Z]+\s=\s\w+(,[A-Z]+\s=\s\w+)*)\sWHERE\s([A-Z]+\s=\s\w+(\s(AND|OR)\s[A-Z]+\s=\s\w+))*;')
+#
 while entrada != 'salir':
 
     entrada = input()
@@ -31,7 +30,7 @@ while entrada != 'salir':
         mach = update.search(entrada)
         tabla = mach.group(1)
         cambios = mach.group(2).split(',')
-        cond = mach.group(4).split(',')
+        cond = mach.group(4)
 
-    else:
+    elif entrada != 'salir':
         print('Error de sintaxis. Comando no valido.')
