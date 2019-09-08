9c1c6eb7c1f16a35dc9d506e4625896a68aadc5d
Bitovito <44344453+Bitovito@users.noreply.github.com>
2019-09-08 18:26:47 -0300

    finall
    quite flags y agregue una pequeña descripción

--- regEx.py
+++ regEx.py
@@ -2,7 +2,7 @@ import re
 from funciones import *
 
 entrada = 'xS'
-###
+# A continuacion divido la expresion regular en distintos trozos para mayor facilidad de lectura
 s = r'SELECT *([^\x20]+ *(?: *, *[^\x20]+ *)*) *FROM ([^\x20]+) *;?'
 i = r'(?: *INNER *JOIN *([^\x20]+);?)'
 w = r'(?: *WHERE *((?:[^\x20]+ *= *\'[^\27]+\'|[^\x20]+ *= *[^\x20]+)(?: *(?:AND|OR) *(?:[^\x20]+ *= *\'[^\27]+\'|[^\x20]+ *= *[^\x20]+))*);?)'#les quite el ()?     #Le puse un :? al (AND|OR)
@@ -17,63 +17,60 @@ u = r'UPDATE *([^\x20]+) *SET *([^\x20]+ *= *[\x00-\x7F]+(?:, *[^\x20]+ *= *[\x0
 wh = r' *WHERE *([^\x20]+ *= *[\x00-\x7F]+(?: *(?:AND|OR) *[^\x20]+ *= *[\x00-\x7F]+)*);'
 update = re.compile(u+wh)
 ###
+# Ahora hago un ciclo while donde uno puede ingresar su Query mediante el input,
+# esto revisa si la query ingresada pertenece a los tipos propuestos, luego depuro
+# los datos y los envio a las funciones respectivas.
 while entrada != 'salir':
     mach = 0
     entrada = input()
-
-    if re.match(select,entrada):###### OJO QUE ESTA WEA FIJO TIRA ERROR, Y ACTUALMENTE NADA ES OPCIONAL
+# Depuro los datos de SELECT, los datos opcionales como INNER JOIN 
+# los paso a una lista, donde si no es pedido se marca como una x para
+# evitar problemas con el paso de parametros.
+# Si no hay condicion cuando se pide un INNER JOIN tira error.
+    if re.match(select,entrada):
         mach = select.search(entrada)
         columnas = re.match(s,entrada).group(1).replace(' ','').split(',')#Lista de strings
-        print('Columnas: ',columnas)
         tabla = re.match(s,entrada).group(2).strip(';')#Un string
-        print('Tabla: ',tabla)
         otros = []
         if re.search(i, entrada):
             union = re.search(re.compile(i), entrada).group(1).strip(';')#Un string
-            print('Union: ',union)
             otros.append(union)
         else:
             otros.append('x')
         if re.search(w, entrada):
             conds = re.search(re.compile(w), entrada).group(1).strip(';')#Lista de listas, cada sub-lista es un or y en cada una van los ANDs (Nombre = algo)
             c = condSplit(conds)
-            print('Condiciones: ',c)
             otros.append(c)
         else:
             otros.append('x')
         if re.search(o, entrada):
             orden = re.search(re.compile(o), entrada).group(1).strip(';')#Un string
-            print('Orden: ',orden)
             otros.append(orden)
         else:
             otros.append('x')
-        print(otros)
-        Select(columnas,tabla,otros)
+        if otros[0] != 'x' and otros[1] == 'x':
+            print('Error de sintaxis. Comando no valido.')
+        else:
+            Select(columnas,tabla,otros)
 
-
+# Depuro los datos de INSERT, si la cantidad de valores es
+# mayor que las columnas a insertar tira error.
     if insert.search(entrada):
         mach = insert.search(entrada)
         tabla = mach.group(1)
-        print(tabla)
         columnas = mach.group(2).replace('(','').replace(')','').split(',')
-        print(columnas)
         valores = mach.group(4).replace('(','').replace(')','').split(',')
-        print(valores)
-        #¿Cambio los tipos de valores?
         if len(columnas) != len(valores):
             print('Error de sintaxis. Comando no valido.')
         INSERT(tabla,columnas,valores)
-
+# Depuro los datos en update.
     elif update.search(entrada):
         mach = re.search(update, entrada)
         tabla = re.search(update, entrada).group(1)#Un string
-        print('Tabla: ',tabla)
         cambios = re.search(update, entrada).group(2).split(',')#Lista de strings (Nomnre = algo)
-        print('Cambios: ',cambios)
         conds = re.search(update, entrada).group(3)#Lista de listas, cada sub-lista es un or y en cada una van los ANDs (Nombre = algo)
         c = condSplit(conds)
-        print('Condiciones: ',c)
         UPDATE(tabla,cambios,c)
-
+# Si la query es invalida y distinta de salir lanza mensaje de error.
     elif not mach and entrada!='salir':
         print('Error de sintaxis. Comando no valido.')
